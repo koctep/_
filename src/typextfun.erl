9d5506b31efbc2c7df8eff3b5af1e209b43cd4d3
Ilya Ashchepkov <koctep@gmail.com>
2013-10-13 18:32:47 +0700

    init

---
+++ src/typextfun.erl
@@ -0,0 +1,11 @@
+-module(typextfun).
+
+%-export([to_hex/1]).
+%-export([from_hex/1]).
+%-export([reverse/1]).
+
+%-export([binary_to_hex/1]).
+%-export([binary_from_hex/1]).
+%-export([binary_reverse/1]).
+
+
